1bb8737df41a68812bb273f017cb96966f63741f
Vladislav Pavlov <vldslw@gmail.com>
2023-09-22 19:29:20 -0300

    feat: add backend request function on submit form

--- components/Form.vue
+++ components/Form.vue
@@ -19,7 +19,11 @@
 </template>
 
 <script>
+// here we import nanoid to generate random slugs, but we would normally do it in the backend if we had one
 import { nanoid } from "nanoid";
+
+// here we import axios, but we won't actually use it, since we don't have a backend to handle the request at this time
+import axios from "axios";
 
 export default {
   data() {
@@ -30,6 +34,8 @@ export default {
     };
   },
   methods: {
+    // this is example code, because there is no backend to handle the request at this time,
+    // so I just did it to make the app look like it is working
     onButtonClick() {
       if (this.slug === "") {
         // generate random slug if slug is empty
@@ -44,6 +50,29 @@ export default {
         path: "/shortened",
         query: { longUrl: this.longUrl, shortUrl: this.shortUrl },
       });
+    },
+    // this is how the request should have looked like with backend active
+    async onButtonClickAsync() {
+      try {
+        const response = await axios.post(
+          "https://shrtnr.vercel.app/api/shorten",
+          {
+            longUrl: this.longUrl,
+            // generating random slug if slug is empty would be done in the backend instead of the frontend,
+            // so here we would just pass the slug as it is, wheter it is empty or not
+            slug: this.slug,
+          },
+        );
+        // here we would get the shortUrl from the backend response
+        this.shortUrl = response.data.shortUrl;
+        // when we would redirect to /shortened route with query params just as we did in the example code
+        this.$router.push({
+          path: "/shortened",
+          query: { longUrl: this.longUrl, shortUrl: this.shortUrl },
+        });
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
